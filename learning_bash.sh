5d26f1e17a2309dab79e2b08ba6da4db826ecba2
Benecia-A <benecia.angelica@wmich.edu>
2025-01-23 17:52:24 -0500

    Add files via upload

---
+++ learning_bash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+greeting=Hi
+name=Benecia
+echo "$greeting $name"
+echo "This script is helping you learn bash."
+
+cd /Users/benecia/Desktop
+mkdir bioinformatics
+
+cd /Users/benecia/Desktop/bioinformatics
+mkdir bash_scripts
+
+mv /Users/benecia/Desktop/bash_template.txt /Users/benecia/Desktop/bioinformatics/bash_scripts
+mv /Users/benecia/Desktop/bioinformatics/bash_scripts/bash_template.txt /Users/benecia/Desktop/bioinformatics/bash_scripts/bash_template.sh
+#Add your commands below!
